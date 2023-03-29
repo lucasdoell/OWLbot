8dfb9ba80df516494716a0b5a140cf31b4624c56
Lucas Doell <lmd248@cornell.edu>
2023-03-28 21:54:43 -0400

    remove jsdom

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer-core";
 import chrome from "chrome-aws-lambda";
-import { JSDOM } from "jsdom";
+// import { JSDOM } from "jsdom";
 import chalk from "chalk";
 import express from "express";
 
@@ -27,9 +27,10 @@ app.get("/api/schedule", async (req, res) => {
     "https://overwatchleague.com/en-us/schedule"
   );
 
-  const data = extractData(document);
+  // const data = extractData(document);
 
-  res.send(`Schedule: ${data}`);
+  // res.send(`Schedule: ${data}`);
+  res.send(document);
 });
 
 async function fetchPage(url: string) {
@@ -37,7 +38,7 @@ async function fetchPage(url: string) {
     process.env.NODE_ENV === "production"
       ? {
           args: chrome.args,
-          executablePath: { executablePath: "google-chrome-stable" },
+          executablePath: await chrome.executablePath,
           headless: chrome.headless,
         }
       : {
@@ -56,30 +57,30 @@ async function fetchPage(url: string) {
   return await page.content();
 }
 
-function extractData(data: string) {
-  const dom = new JSDOM(data);
-  const document = dom.window.document;
+// function extractData(data: string) {
+//   const dom = new JSDOM(data);
+//   const document = dom.window.document;
 
-  let week = document.querySelector(
-    ".schedule-boardstyles__Title-j4x5cc-4.iglKJd"
-  );
+//   let week = document.querySelector(
+//     ".schedule-boardstyles__Title-j4x5cc-4.iglKJd"
+//   );
 
-  let schedule: HTMLElement[] = Array.from(
-    document.querySelectorAll(
-      ".schedule-boardstyles__ContainerCards-j4x5cc-8.jcvNlt"
-    )
-  );
+//   let schedule: HTMLElement[] = Array.from(
+//     document.querySelectorAll(
+//       ".schedule-boardstyles__ContainerCards-j4x5cc-8.jcvNlt"
+//     )
+//   );
 
-  const matchDayRegex =
-    /\b(?:MON|TUE|WED|THU|FRI|SAT|SUN), (?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) d{1,2}\b/;
+//   const matchDayRegex =
+//     /\b(?:MON|TUE|WED|THU|FRI|SAT|SUN), (?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) d{1,2}\b/;
 
-  const matchTimeRegex = /\b(?:\d{1,2}:\d{2} [AP]M|Final)\b/;
+//   const matchTimeRegex = /\b(?:\d{1,2}:\d{2} [AP]M|Final)\b/;
 
-  // For each item in schedule, get the match day and time
-  const matches: string[] = [];
-  for (let i = 0; i < schedule.length; i++) {
-    matches.push(schedule[i].children[0].textContent!);
-  }
+//   // For each item in schedule, get the match day and time
+//   const matches: string[] = [];
+//   for (let i = 0; i < schedule.length; i++) {
+//     matches.push(schedule[i].children[0].textContent!);
+//   }
 
-  console.log(matches);
-}
+//   console.log(matches);
+// }
