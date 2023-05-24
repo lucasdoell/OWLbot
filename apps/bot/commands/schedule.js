b7fdccee0a3c15368fe1fd5b0678a84a744461fd
Lucas Doell <lmd248@cornell.edu>
2023-05-24 18:10:20 -0400

    add new schedule slash command

---
+++ apps/bot/commands/schedule.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("schedule")
+    .setDescription("Returns the schedule for the current week"),
+  async execute(interaction) {
+    const data = await axios.get("https://owl-api.lux.dev/api/schedule/cache");
+    const embed = new EmbedBuilder()
+      .setColor(0x009dcf)
+      .setTitle("Schedule")
+      .setURL("https://lux.dev")
+      .addFields(
+        data.data.matches.map((match) => {
+          return {
+            name: `${match.team1} vs ${match.team2}`,
+            value: `${match.time} on ${match.date}`,
+            inline: true,
+          };
+        })
+      )
+      .setAuthor({
+        name: "OWL Bot",
+        iconUrl: "https://avatars.githubusercontent.com/u/55987186",
+        url: "https://lux.dev",
+      })
+      .setDescription(
+        "The schedule for the current week. All times are in EST."
+      )
+      .setFooter({
+        text: "Written by lux#5470",
+        iconUrl: "https://avatars.githubusercontent.com/u/55987186",
+      })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
