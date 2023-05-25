a62b7ea609ff292561021ee3959355b4f52eca59
Lucas Doell <lmd248@cornell.edu>
2023-05-24 22:15:03 -0400

    update next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async redirects() {
+    return [
+      {
+        source: "/invite",
+        destination:
+          "https://discord.com/api/oauth2/authorize?client_id=1088622741558669383&permissions=2147485696&scope=bot%20applications.commands",
+        permanent: true,
+      },
+    ];
+  },
 };
